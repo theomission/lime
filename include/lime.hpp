5adb7ffec635e4c084207e461f44bf8260d78222
tatsy <tatsy.mail@gmail.com>
2015-02-02 19:35:01 +0900

    Create github page.

--- include/lime.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _LIBRARY_FOR_IMAGE_EDITING_H_
-#define _LIBRARY_FOR_IMAGE_EDITING_H_
-
-#include "../src/npr/lime_npr.hpp"
-#include "../src/core/lime_core.hpp"
-#include "../src/hvs/lime_hvs.hpp"
-
-#endif
--- src/hvs/lime_hvs.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef _LIME_HVS_HPP_
-#define _LIME_HVS_HPP_
-
-#include "color_constancy.h"
-
-#endif /* _LIME_HVS_HPP_ */
